4e5df7ea4172df2e65689e0f2374ace34b113962
yhli365 <yhli365@126.com>
2018-10-09 17:45:02 +0800

    eshadoop/ch01 config dynamic

--- eshadoop/ch01/src/main/java/com/packtpub/esh/Driver.java
+++ eshadoop/ch01/src/main/java/com/packtpub/esh/Driver.java
@@ -1,25 +1,44 @@
 package com.packtpub.esh;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 import org.elasticsearch.hadoop.mr.EsOutputFormat;
 
+import java.io.File;
 
-public class Driver {
+public class Driver extends Configured implements Tool {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        File file = new File("eshadoop-site.xml");
+        if (file.exists()) {
+            conf.addResource(file.toURI().toURL());
+        }
+        ToolRunner.run(conf, new Driver(), args);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+
         // ElasticSearch Server nodes to point to
-        conf.set("es.nodes", "10.10.8.111:9200"); //localhost:9200
+        conf.setIfUnset("es.nodes", "localhost:9200");
         // ElasticSearch index and type name in {indexName}/{typeName} format
-        conf.set("es.resource", "eshadoop/wordcount");
+        conf.setIfUnset("es.resource", "eshadoop/wordcount");
+        System.out.println("<conf> es.nodes = " + conf.get("es.nodes"));
+        System.out.println("<conf> es.resource = " + conf.get("es.resource"));
 
         // Create Job instance
-        Job job = new Job(conf, "word count");
+        Job job = Job.getInstance(conf);
+        job.setJobName("word count");
+
         // set Driver class
         job.setJarByClass(Driver.class);
         job.setMapperClass(WordsMapper.class);
@@ -32,6 +51,8 @@ public class Driver {
         FileInputFormat.addInputPath(job, new Path(args[0]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        return 0;
     }
 
 }
--- eshadoop/my.md
+++ eshadoop/my.md
@@ -20,14 +20,39 @@ hadoop-2.6.0-cdh5.14.0-native.tar.gz
 ```
 mvn clean
 mvn compile
+mvn dependency:sources
 mvn package
 ```
 
 # Content
 =============================
-## ch01
+## settings
+```
+$ vi ~/conf/eshadoop-site.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+  <property>
+    <name>es.nodes</name>
+    <value>10.10.8.111:9200</value>
+  </property>
+</configuration>
+```
+
+## review
+```
+#ch02p27
+EsInputFormat/EsOutputFormat/es.input.json=yes
+```
+
+## ch01 环境部署
 ```
 $ hdfs dfs -mkdir -p /eshadoop/input
 $ hdfs dfs -put ch01 /eshadoop/input
 $ hadoop jar ch01-0.0.1-job.jar /eshadoop/input/ch01/sample.txt
+$ hadoop jar ch01-0.0.1-job.jar -Des.nodes=10.10.8.111:9200 /eshadoop/input/ch01/sample.txt
+```
+
+## ch02 初识ES-Hadoop
+```
+$ 
 ```
